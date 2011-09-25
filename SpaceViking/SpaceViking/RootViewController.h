4782517f050934d19d59214f578b428d5e61ed4e
Naveed Siddiqui <naveed@10eighteen.com>
2011-09-25 22:14:12 +0800

    First commit - project setup

---
+++ SpaceViking/SpaceViking/RootViewController.h
@@ -0,0 +1,16 @@
+//
+//  RootViewController.h
+//  SpaceViking
+//
+//  Created by Muhammad Naveed Siddiqui on 25/9/11.
+//  Copyright __MyCompanyName__ 2011. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface RootViewController : UIViewController {
+
+}
+
+@end
